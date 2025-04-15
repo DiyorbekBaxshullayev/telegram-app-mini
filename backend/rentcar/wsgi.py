aa6d2f5f74996803153bcd0e170cdf59bba49b26
DiyotbekBaxshullayev <baxshullayevd4@gmail.com>
2025-04-15 22:42:21 +0500

    5

--- backend/rentcar/wsgi.py
+++ backend/rentcar/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for rentcar project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = '/home/your-username/your-project-folder'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'your_project.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rentcar.settings')
-
 application = get_wsgi_application()
