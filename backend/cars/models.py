b8c7027e88fcb5725bb92b35cf77b8b1db4d0f41
DiyotbekBaxshullayev <baxshullayevd4@gmail.com>
2025-04-15 22:09:37 +0500

    5

--- backend/cars/models.py
+++ backend/cars/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 
 
-models.py
 class Car(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
@@ -16,7 +15,6 @@ class Car(models.Model):
     def __str__(self):
         return self.name
 
-models.py
 
 # class Car(models.Model):
 #     title = models.CharField(max_length=100)
