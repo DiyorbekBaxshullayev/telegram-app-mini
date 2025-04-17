1903e43b79c0b10925fa9fe707850e079fc77a99
DiyotbekBaxshullayev <baxshullayevd4@gmail.com>
2025-04-17 13:35:13 +0500

    7

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_BASE = "DiyorbekBaxshullayev.pythonanywhere.com "; // Django backend URL
+const API_BASE = "DiyorbekBaxshullayev.pythonanywhere.com/api"; // Django backend URL
 
-export const getCars = () => axios.get("DiyorbekBaxshullayev.pythonanywhere.com /cars/");
+export const getCars = () => axios.get("DiyorbekBaxshullayev.pythonanywhere.com/api/cars/");
 // export const getCars = () => axios.get(`${API_BASE}/cars/`);
 export const getCarDetail = (id) => axios.get(`${API_BASE}/cars/${id}/`);
 export const createOrder = (data) => axios.post(`${API_BASE}/orders/`, data);
