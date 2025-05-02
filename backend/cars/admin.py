f13a2fba65aa09762c567b12bfce461f491351ec
DiyotbekBaxshullayev <baxshullayevd4@gmail.com>
2025-05-02 14:39:50 +0500

    6

--- backend/cars/admin.py
+++ backend/cars/admin.py
@@ -1,3 +1,20 @@
+# from django.contrib import admin
+# from .models import Car, Order
+
+# @admin.register(Car)
+# class CarAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'brand', 'year', 'daily_price', 'is_active')
+
+# @admin.register(Order)
+# class OrderAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'car', 'phone', 'status', 'created_at')
+#     list_filter = ('status', 'created_at')
+#     search_fields = ('name', 'phone', 'passport')
+
+
+
+
+
 from django.contrib import admin
 from .models import Car, Order
 
