80371ab235fb3883e9c9be3ac3d1713be7f1394a
DiyorbekBaxshullayev <baxshullayevd4@gmail.com>
2025-05-12 10:28:33 +0500

    1

--- backend/cars/utils.py
+++ backend/cars/utils.py
@@ -1,9 +1,12 @@
-# cars/utils.py
+import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def send_telegram_message(message: str):
-    BOT_TOKEN = '6176561737:AAGCeN60gXcjIjYNImgzzKSfMh6gsZItJMs'
-    CHAT_ID = '2007510369'
+    BOT_TOKEN = os.getenv('BOT_TOKEN')
+    CHAT_ID = os.getenv('CHAT_ID')
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {
         'chat_id': CHAT_ID,
