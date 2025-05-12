5a8c90943ea9955d4e069fa49a03199749bf8a7f
DiyorbekBaxshullayev <baxshullayevd4@gmail.com>
2025-05-12 14:22:08 +0500

    4

--- backend/rentcar/settings.py
+++ backend/rentcar/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('6176561737:AAGz9_Gp4CiAlCpTW0PQuD6lqFdQvkEZFMc')
+CHAT_ID = os.getenv('2007510369')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
