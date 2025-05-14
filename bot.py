12cbf4d71185cf047caa60e053c75a8d72e66219
DiyorbekBaxshullayev <baxshullayevd4@gmail.com>
2025-05-14 17:15:10 +0500

    .

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, WebAppInfo
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# start komandasi handler
+# /start komandasi
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [InlineKeyboardButton("Rent a Car 🚗", web_app=WebAppInfo(url="https://telegram-app-mini.vercel.app/"))]
+        [InlineKeyboardButton("🚘 Ijaraga olish", web_app=WebAppInfo(url="https://rentcar-front.vercel.app/"))],
+        [InlineKeyboardButton("📍 Manzilni ko‘rish", web_app=WebAppInfo(url="https://rentcar-map.vercel.app/"))],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Ijaraga olish yoki manzilimizni ko‘rish uchun tugmani bosing 👇", reply_markup=reply_markup)
 
-    await update.message.reply_text("Ijaraga olish uchun tugmani bosing 👇", reply_markup=reply_markup)
-
+# Botni ishga tushirish
 app = ApplicationBuilder().token("").build()
 app.add_handler(CommandHandler("start", start))
 
